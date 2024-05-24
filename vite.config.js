4b5709c50612c4d863979b81ca6fe6dbd4d81466
jshuo7777 <166576199+jshuo7777@users.noreply.github.com>
2024-05-24 21:41:02 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test-for-vue/',
+  base: "/test-for-vue/",
   plugins: [
     vue(),
   ],
