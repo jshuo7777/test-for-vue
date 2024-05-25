37b89709134f07212fdc91e0b52b68aa8d4c16dc
zenco <wuwo@gmail.com>
2024-05-25 16:06:26 +0800

    unot-test

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -3,9 +3,26 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
 
+// describe('HelloWorld', () => {
+//   it('renders properly', () => {
+//     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+//     expect(wrapper.text()).toContain('Hello Vitest')
+//   })
+// })
+
 describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+  it('renders properly with default props', () => {
+    const wrapper = mount(HelloWorld);
+    expect(wrapper.text()).toContain('Hello World');
+  });
+
+  it('renders properly with custom msg prop', () => {
+    const wrapper = mount(HelloWorld, {
+      props: {
+        msg: 'Hello Vitest',
+      },
+    });
+    // Assert something that is not true
+    expect(wrapper.text()).toContain('Incorrect Message');
+  });
+});
